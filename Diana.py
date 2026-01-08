db368280f5f792d12759dff5919cbc01f9887bdc
diana704-ship-it <francodiana704@gmail.com>
2026-01-08 11:54:59 -0600

    Create Diana.py

---
+++ Diana.py
@@ -0,0 +1,26 @@
+print("Diana Iveet Nava Franco")
+
+print("Fortalezas:")
+print("Respetuosa")
+print("Amable")
+print("Responsable")
+
+print("Debilidades:")
+print("Me distraigo fácilmente")
+print("A veces soy impaciente")
+
+print("Gustos:")
+print("Escuchar música")
+print("Ver series")
+
+print("Canciones favoritas:")
+print("Coldplay- Paradise")
+
+print("Artistas favoritos:")
+print("No tengo uno en específico")
+
+print("Resumen de actividades en vacaciones:")
+print("Durante vacaciones realize actividades productivas como hacer quehaceres y en las tardes salia a correr, también convivi con mi familia.")
+
+print("Estado de ánimo actual:")
+print("Desmotivada y tranquila")
